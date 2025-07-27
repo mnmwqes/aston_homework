36d134925887550471bbbc0c36e17fef8c3002bf
mnmwqes <nargiza150300@gmail.com>
2025-07-27 23:06:46 +0300

    Create Bear.java

---
+++ homework_1/src/types/Bear.java
@@ -0,0 +1,21 @@
+package types;
+
+import animals.Mammal;
+import interfaces.HasWool;
+
+public class Bear extends Mammal implements HasWool {
+
+    public void move() {
+        System.out.println("Медведь двигается.");
+    }
+
+    @Override
+    public void feedWithMilk() {
+        System.out.println("Медведь кормит молоком.");
+    }
+
+    @Override
+    public void describeWool() {
+        System.out.println("Медведь имеет шерсть.");
+    }
+}

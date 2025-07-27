cc4f1889f4031e80964890016c4b8bcbb84f2a29
mnmwqes <nargiza150300@gmail.com>
2025-07-28 02:09:50 +0300

    Update Whale.java

--- homework_1/src/types/Whale.java
+++ homework_1/src/types/Whale.java
@@ -2,8 +2,9 @@ package types;
 
 import animals.Mammal;
 import interfaces.LivesInWater;
+import interfaces.hasASpine;
 
-public class Whale extends Mammal implements LivesInWater  {
+public class Whale extends Mammal implements hasASpine, LivesInWater  {
 
     public void move() {
         System.out.println("Кит двигается.");
@@ -14,12 +15,15 @@ public class Whale extends Mammal implements LivesInWater  {
         System.out.println("Кит кормит детёнышей молоком.");
     }
 
-    public void haveASpine() {
-        System.out.println("Кит имеет позвоночник.");
+    @Override
+    public void describeTheSpine() {
+        System.out.println("Кит имеет позвоночник, состоящий из 4-х " +
+                "отделов: шейный, грудной, поясничный и хвостовой.");
     }
 
     @Override
     public void toSwim() {
         System.out.println("Кит обитает в воде.");
+        System.out.println();
     }
 }

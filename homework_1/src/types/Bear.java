2796434b132b08aa27fbd23ffbf71075969f4eec
mnmwqes <nargiza150300@gmail.com>
2025-07-28 02:09:41 +0300

    Update Bear.java

--- homework_1/src/types/Bear.java
+++ homework_1/src/types/Bear.java
@@ -1,9 +1,10 @@
 package types;
 
 import animals.Mammal;
+import interfaces.hasASpine;
 import interfaces.HasWool;
 
-public class Bear extends Mammal implements HasWool {
+public class Bear extends Mammal implements hasASpine, HasWool {
 
     public void move() {
         System.out.println("Медведь двигается.");
@@ -15,7 +16,15 @@ public class Bear extends Mammal implements HasWool {
     }
 
     @Override
+    public void describeTheSpine() {
+        System.out.println("Медведь имеет позвоночник, состоящий из " +
+                "5-х отделов: шейный, грудной, поясничный, " +
+                "крестцовый и хвостовой.");
+    }
+
+    @Override
     public void describeWool() {
         System.out.println("Медведь имеет густую шерсть.");
+        System.out.println();
     }
 }

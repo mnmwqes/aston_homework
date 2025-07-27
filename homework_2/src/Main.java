acd6a97e16dbe6135c15173fbb992b70995dd7ca
mnmwqes <nargiza150300@gmail.com>
2025-07-28 01:15:29 +0300

    Create Main.java

---
+++ homework_2/src/Main.java
@@ -0,0 +1,38 @@
+import types.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Airplane airplane = new Airplane();
+        airplane.drive();
+        airplane.describeTheWheels();
+        airplane.describeTheScrew();
+        airplane.describeTheWings();
+        airplane.describeTheCargo();
+
+        Helicopter helicopter = new Helicopter();
+        helicopter.drive();
+        helicopter.describeTheWheels();
+        helicopter.describeTheScrew();
+        helicopter.describeTheCargo();
+
+        Boat boat = new Boat();
+        boat.drive();
+        boat.describeTheScrew();
+        boat.describeTheCargo();
+
+        Tanker tanker = new Tanker();
+        tanker.drive();
+        tanker.describeTheScrew();
+        tanker.describeTheCargo();
+
+        Truck truck = new Truck();
+        truck.drive();
+        truck.describeTheWheels();
+        truck.describeTheCargo();
+
+        Taxi taxi = new Taxi();
+        taxi.drive();
+        taxi.describeTheWheels();
+        taxi.describeTheCargo();
+    }
+}

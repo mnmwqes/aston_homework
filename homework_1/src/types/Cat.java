f0baad81741502e2da4f6cc533b6b791c629e788
mnmwqes <nargiza150300@gmail.com>
2025-07-28 02:09:43 +0300

    Update Cat.java

--- homework_1/src/types/Cat.java
+++ homework_1/src/types/Cat.java
@@ -2,8 +2,9 @@ package types;
 
 import animals.Mammal;
 import interfaces.HasWool;
+import interfaces.hasASpine;
 
-public class Cat extends Mammal implements HasWool {
+public class Cat extends Mammal implements hasASpine, HasWool {
 
     public void move() {
         System.out.println("Кошка двигается.");
@@ -11,11 +12,18 @@ public class Cat extends Mammal implements HasWool {
 
     @Override
     public void feedWithMilk() {
-        System.out.println("Кошка кормит молоком.");
+        System.out.println("Кошка кормит детенышей молоком.");
+    }
+
+    @Override
+    public void describeTheSpine() {
+        System.out.println("Кошка имеет позвоночник, состоящий из 4-х " +
+                "отделов: шейный, грудной, поясничный и хвостовой.");
     }
 
     @Override
     public void describeWool() {
-        System.out.println("Кошка имеет гладкую шерсть.");
+        System.out.println("Кошка имеет гладкую шерсть с различными окрасами.");
+        System.out.println();
     }
 }

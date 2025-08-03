afaf329d3893d57c643481c2d83fc7887b0ae270
mnmwqes <nargiza150300@gmail.com>
2025-08-04 00:29:11 +0300

    2

---
+++ homework_2/src/Main.java
@@ -0,0 +1,29 @@
+import model.Student;
+import model.Book;
+import utils.DataGenerator;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Student> students = DataGenerator.generateStudents(10);
+
+        Optional<Book> result = students.stream()
+                .peek(System.out::println)
+                .flatMap(student -> student.getBooks().stream())
+                .sorted(Comparator.comparingInt(Book::getPages))
+                .distinct()
+                .filter(book -> book.getYear() > 2000)
+                .limit(3)
+                .map(book -> {
+                    System.out.println("Год: " + book.getYear());
+                    return book;
+                })
+                .findFirst();
+
+        System.out.println(result.map(b -> "Первая подходящая книга: " + b)
+                .orElse("Книга не найдена"));
+    }
+}
